6fb03718a2cfdd088df25ff9656525cad0d4d30a
Sowmyatorra <sowmyatorra@gmail.com>
2024-11-11 15:47:47 +0530

    okay

--- program1.py
+++ program1.py
@@ -1,10 +1,18 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        
+        bracket_map= {')':'(','}':'{',']':'['}
+        stack=[]
+        for char in s:
+            if char in bracket_map:
+                if stack and stack[-1]==bracket_map[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
 
 
 
